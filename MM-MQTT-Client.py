d070669776e47fa5942bd6d1c3af92564c231bd0
Adam Forch <adam.forch@gmail.com>
2018-12-01 16:42:16 +0100

    Updated with config file

--- MM-MQTT-Client.py
+++ MM-MQTT-Client.py
@@ -5,20 +5,21 @@
 
 import paho.mqtt.client as mqtt
 from subprocess import call
+from configparser import ConfigParser
+import os
 
-#actual username, password, MQTT server address, and topic here
-mqtt_name = "user" 
-mqtt_pass = "password"
-mqtt_server = "1.2.3.4"
-mqtt_topic = "home/my_topic"
-
+#Config file setup and read
+config = ConfigParser(delimiters=('=', ))
+config.optionxform = str
+config.read('MM-MQTT.ini')
+    
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
 
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
-    client.subscribe(mqtt_topic)
+    client.subscribe(config.get('homeassistant', 'mqtt_topic'))
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
@@ -32,13 +33,16 @@ def on_message(client, userdata, msg):
 
         
 if __name__== "__main__":
+    
+    
+    #MQTT Client setup
     client = mqtt.Client()
     client.on_connect = on_connect
     client.on_message = on_message
-    client.username_pw_set(mqtt_name, mqtt_pass)
+    client.username_pw_set(config.get('broker', 'mqtt_user'), config.get('broker','mqtt_passwd'))
 
-    client.connect(mqtt_server, 1883, 60)
-
+    #MQTT Connection and loop
+    client.connect(config.get('broker', 'mqtt_server'), 1883, 60)
 
     client.loop_forever()
 
